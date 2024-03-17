a89e9336ca8eea2f8f9af59ec8a447e677525855
elena-aversa <97602287+elena-aversa@users.noreply.github.com>
2024-03-17 19:34:48 +0100

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Function to make SVG images clickable
+    function makeClickable() {
+        var images = document.querySelectorAll('svg image[data-url]');
+        images.forEach(function(img) {
+            img.style.cursor = 'pointer'; // Change cursor on hover
+            img.addEventListener('click', function() {
+                var url = img.getAttribute('data-url'); // Get the URL
+                window.open(url, '_blank'); // Open the URL in a new tab
+            });
+        });
+    }
+
+    makeClickable();
+});
